8a98ef9b59484800eb0062e9b66bccb36d18ee89
Enes Genç <hello@enesgenc.dev>
2025-10-15 20:16:18 +0300

    feat: setup routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,10 +1,20 @@
 package router
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/enxg/skyticket/internal/controllers"
+	"github.com/gofiber/fiber/v3"
+)
 
 type Controllers struct {
+	EventController controllers.EventController
 }
 
 func SetupRoutes(app *fiber.App, c Controllers) {
+	app.Group("/events").
+		Post("", c.EventController.CreateEvent).
+		Get("/:id", c.EventController.GetEventByID).
+		Get("", c.EventController.GetAllEvents).
+		Patch("/:id", c.EventController.UpdateEvent).
+		Delete("/:id", c.EventController.DeleteEvent)
 
 }
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"os"
+
+	"github.com/enxg/skyticket/internal/controllers"
+	"github.com/enxg/skyticket/internal/repositories"
 	"github.com/enxg/skyticket/internal/router"
+	"github.com/enxg/skyticket/internal/services"
 	"github.com/enxg/skyticket/pkg/validator"
 	"github.com/gofiber/fiber/v3"
 	"github.com/rs/zerolog/log"
@@ -10,7 +15,6 @@ import (
 )
 
 func main() {
-	app := fiber.New()
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal().Msg("MONGODB_URI environment variable not set")
@@ -23,12 +27,19 @@ func main() {
 
 	db := client.Database("skyticket")
 
+	eventRepository := repositories.NewEventRepository(db)
+	eventService := services.NewEventService(eventRepository)
+	eventController := controllers.NewEventController(eventService)
 
 	app := fiber.New(fiber.Config{
 		StructValidator: validator.NewStructValidator(),
 	})
 
-	err := app.Listen(":3000")
+	router.SetupRoutes(app, router.Controllers{
+		EventController: eventController,
+	})
+
+	err = app.Listen(":3000")
 	if err != nil {
 		log.Fatal().Err(err).Msg("error starting server")
 	}
