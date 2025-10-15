4e32d9d6fe4f6ce79a837eee00be4941b16b9449
Enes Genç <hello@enesgenc.dev>
2025-10-15 19:02:59 +0300

    feat: setup fiber app

---
+++ internal/router/router.go
@@ -0,0 +1,10 @@
+package router
+
+import "github.com/gofiber/fiber/v3"
+
+type Controllers struct {
+}
+
+func SetupRoutes(app *fiber.App, c Controllers) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/enxg/skyticket/internal/router"
+	"github.com/gofiber/fiber/v3"
+	"github.com/rs/zerolog/log"
+)
 
+func main() {
+	app := fiber.New()
+
+	router.SetupRoutes(app, router.Controllers{})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal().Err(err).Msg("error starting server")
+	}
 }
