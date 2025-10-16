dd9aab788c83999b910fc3b6dfc99adc90c0b92f
Enes Genç <hello@enesgenc.dev>
2025-10-16 15:28:28 +0300

    feat: add example values to structs

--- internal/models/event.go
+++ internal/models/event.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Event struct {
-	ID    bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Name  string        `json:"name,omitempty" bson:"name,omitempty"`
-	Date  time.Time     `json:"date,omitempty" bson:"date,omitempty"`
-	Venue string        `json:"venue,omitempty" bson:"venue,omitempty"`
+	ID    bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty" example:"68f0c6a8f5673dc0ec646731"`
+	Name  string        `json:"name,omitempty" bson:"name,omitempty" example:"FIBA EuroBasket 2025 Finali"`
+	Date  time.Time     `json:"date,omitempty" bson:"date,omitempty" example:"2025-09-14T21:00:00+03:00"`
+	Venue string        `json:"venue,omitempty" bson:"venue,omitempty" example:"YTÜ Davutpaşa Tarihi Hamam"`
 }
--- internal/requests/event.go
+++ internal/requests/event.go
@@ -1,13 +1,13 @@
 package requests
 
 type CreateEventRequest struct {
-	Name  string `json:"name" validate:"required"`
-	Date  string `json:"date" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
-	Venue string `json:"venue" validate:"required"`
+	Name  string `json:"name" validate:"required" example:"FIBA EuroBasket 2025 Finali"`
+	Date  string `json:"date" validate:"required,datetime=2006-01-02T15:04:05Z07:00" example:"2025-09-14T21:00:00+03:00"`
+	Venue string `json:"venue" validate:"required" example:"YTÜ Davutpaşa Tarihi Hamam"`
 }
 
 type UpdateEventRequest struct {
-	Name  string `json:"name" validate:""`
-	Date  string `json:"date" validate:"datetime=2006-01-02T15:04:05Z07:00"`
-	Venue string `json:"venue" validate:""`
+	Name  string `json:"name" validate:"" example:"FIBA EuroBasket 2025 Finali"`
+	Date  string `json:"date" validate:"datetime=2006-01-02T15:04:05Z07:00" example:"2025-09-14T21:00:00+03:00"`
+	Venue string `json:"venue" validate:"" example:"YTÜ Davutpaşa Tarihi Hamam"`
 }
