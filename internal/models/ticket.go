55233973c447730a963f9c6c12371e2be3fd86ee
Enes Genç <hello@enesgenc.dev>
2025-10-18 17:26:50 +0300

    feat: add examples to ticket model

--- internal/models/ticket.go
+++ internal/models/ticket.go
@@ -7,15 +7,16 @@ import (
 type TicketStatus string
 
 const (
-	TicketStatusAvailable TicketStatus = "available"
-	TicketStatusReserved  TicketStatus = "reserved"
+	TicketStatusAvailable TicketStatus = "AVAILABLE"
+	TicketStatusReserved  TicketStatus = "RESERVED"
 )
 
+// TODO: Might add an option to specify currency
+
 type Ticket struct {
-	ID         bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	EventID    bson.ObjectID `json:"event_id,omitempty" bson:"event_id,omitempty"`
-	SeatNumber string        `json:"seat_number,omitempty" bson:"seat_number,omitempty"`
-	Price      float64       `json:"price,omitempty" bson:"price,omitempty"`
-	Status     TicketStatus  `json:"status,omitempty" bson:"status,omitempty"`
+	ID         bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty" example:"68f2ab0516a352dc8f40c543"`
+	EventID    bson.ObjectID `json:"event_id,omitempty" bson:"event_id,omitempty" example:"68f0c6a8f5673dc0ec646731"`
+	SeatNumber string        `json:"seat_number,omitempty" bson:"seat_number,omitempty" example:"A12"`
 	Price      int           `json:"price,omitempty" bson:"price,omitempty" example:"4999"`
+	Status     TicketStatus  `json:"status,omitempty" bson:"status,omitempty" example:"AVAILABLE"`
 }
