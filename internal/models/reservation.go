7e19d8692b9c9653ac78ee6a76ed87b6e8f714d0
Enes Genç <hello@enesgenc.dev>
2025-10-19 18:30:01 +0300

    feat: add examples to reservation model

--- internal/models/reservation.go
+++ internal/models/reservation.go
@@ -14,9 +14,9 @@ const (
 )
 
 type Reservation struct {
-	ID              bson.ObjectID     `json:"id,omitempty" bson:"_id,omitempty"`
-	TicketID        bson.ObjectID     `json:"ticket_id,omitempty" bson:"ticket_id,omitempty"`
-	CustomerName    string            `json:"customer_name,omitempty" bson:"customer_name,omitempty"`
-	ReservationDate time.Time         `json:"reservation_date,omitempty" bson:"reservation_date,omitempty"`
-	Status          ReservationStatus `json:"status,omitempty" bson:"status,omitempty"`
+	ID              bson.ObjectID     `json:"id,omitempty" bson:"_id,omitempty" example:"68f4fea9990e605d6589b5f3"`
+	TicketID        bson.ObjectID     `json:"ticket_id,omitempty" bson:"ticket_id,omitempty" example:"68f2ab0516a352dc8f40c543"`
+	CustomerName    string            `json:"customer_name,omitempty" bson:"customer_name,omitempty" example:"Lewis Hamilton"`
+	ReservationDate time.Time         `json:"reservation_date,omitempty" bson:"reservation_date,omitempty" example:"2025-10-1T15:00:00Z"`
+	Status          ReservationStatus `json:"status,omitempty" bson:"status,omitempty" example:"ACTIVE"`
 }
