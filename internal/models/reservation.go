37892fe9f6ec7619bd94394127b39540a28bf28b
Enes Genç <61084101+enxg@users.noreply.github.com>
2025-10-20 11:32:59 +0300

    fix: update reservation date example to a valid future date

--- internal/models/reservation.go
+++ internal/models/reservation.go
@@ -18,6 +18,6 @@ type Reservation struct {
 	EventID         bson.ObjectID     `json:"event_id,omitempty" bson:"event_id,omitempty" example:"68f0c6a8f5673dc0ec646731" extensions:"x-order=1"`
 	TicketID        bson.ObjectID     `json:"ticket_id,omitempty" bson:"ticket_id,omitempty" example:"68f2ab0516a352dc8f40c543" extensions:"x-order=2"`
 	CustomerName    string            `json:"customer_name,omitempty" bson:"customer_name,omitempty" example:"Lewis Hamilton" extensions:"x-order=3"`
-	ReservationDate time.Time         `json:"reservation_date,omitempty" bson:"reservation_date,omitempty" example:"2025-10-1T15:00:00Z" extensions:"x-order=4"`
+	ReservationDate time.Time         `json:"reservation_date,omitempty" bson:"reservation_date,omitempty" example:"2025-10-19T15:00:00Z" extensions:"x-order=4"`
 	Status          ReservationStatus `json:"status,omitempty" bson:"status,omitempty" example:"ACTIVE" extensions:"x-order=5"`
 }
